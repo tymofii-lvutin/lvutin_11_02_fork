c3aa79e08a46483cf517f3640da1736bdbd9dcbd
Tymofii Lvutin <timlvutin24@gmail.com>
2025-02-11 19:14:04 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 def format_linter_error(error: dict) -> dict:
-    # I'AM HERE
-    pass
-
+        return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
+    }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # I'AM HERE
-    pass
-
-
+        return {
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+        "errors": [format_linter_error(error) for error in errors],
+    }
+    
 def format_linter_report(linter_report: dict) -> list:
-    # I'AM HERE
-    pass
+    return [format_single_linter_file(file, errors) for file, errors in linter_report.items()]
