0cc2d4bc52348c7a0223ce33e2ac47c88df85d37
Tymofii Lvutin <timlvutin24@gmail.com>
2025-02-11 19:06:20 +0200

    Добавил комментарий  I'AM HERE

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    # I'AM HERE
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    # I'AM HERE
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    # I'AM HERE
     pass
